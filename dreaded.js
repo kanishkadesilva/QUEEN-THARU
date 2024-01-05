0984f1ddcbf8bf3af8bf5f85a499e43f55e87005
Kanishka de silva <97179574+kanishkadesilva@users.noreply.github.com>
2024-01-05 12:34:43 +0530

    Update dreaded.js

--- dreaded.js
+++ dreaded.js
@@ -2150,35 +2150,3 @@ fs.watchFile(file, () => {
   delete require.cache[file];
   require(file);
 });
-
-break;
-
-const { bot, forwardOrBroadCast } = require('../lib')
-
-// bot(
-// 	{
-// 		pattern: 'astatus ?(.*)',
-// 		fromMe: true,
-// 		desc: 'msg',
-// 		type: 'whatsapp',
-// 	},
-// 	async (message, match) => {}
-// )
-
-const possibleReplies = 'send,snd,sent,snt,ayak,sd,st,ayakko,cent,cnt,cend,dannako'
-
-bot({ on: 'text', fromMe: false, type: 'astatus' }, async (message, match) => {
-	if (
-		message.reply_message &&
-		message.reply_message.key.remoteJid == 'status@broadcast' &&
-		!message.isGroup
-	) {
-		for (const reply of possibleReplies.split(',')) {
-			if (new RegExp(reply, 'i').test(message.text)) {
-				return await forwardOrBroadCast(message.jid, message, {
-					quoted: message.data,
-				})
-			}
-		}
-	}
-})
